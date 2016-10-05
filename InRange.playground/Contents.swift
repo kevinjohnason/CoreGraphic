2194e44750f8c617cac05655f219002dd4b838d9
Kevin Cheng <kevin@onedrop.today>
2016-10-04 21:28:36 -0400

    in range

--- InRange.playground/Contents.swift
+++ InRange.playground/Contents.swift
@@ -4,40 +4,57 @@ import UIKit
 
 import Darwin
 
+let pi:CGFloat = CGFloat(M_PI)
 
 private func drawBackground(){
     let fillCircleRadius : CGFloat = 130
-    let overlayCircleRadius : CGFloat = 110
-    let innerCircleRadius : CGFloat = 86
+    let innerCircleRadius : CGFloat = 100
     let centerPoint = CGPoint(x: fillCircleRadius,y:fillCircleRadius)
-    let buttonToCenterPointLength = innerCircleRadius + (overlayCircleRadius - innerCircleRadius) / 2
-    let headRadius : CGFloat = (overlayCircleRadius - innerCircleRadius) / 2
-    let headY : CGFloat = fillCircleRadius - buttonToCenterPointLength
     let size = CGSize(width: fillCircleRadius * 2, height: fillCircleRadius * 2)
     let pi:CGFloat = CGFloat(M_PI)
     UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
     let fullCirclePath = UIBezierPath(ovalIn: CGRect(origin: CGPoint.zero, size: size))
-    let glucoseColor = UIColor(red: 251/255.0, green: 0, blue: 105/255.0, alpha: 1)
-    glucoseColor.setFill()
+    let bottomColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1)
+    bottomColor.setFill()
     fullCirclePath.fill()
-    let start = CGFloat(-1 * M_PI_2)
-    let decimalInput = 0.25
-    let end = start + CGFloat(2 * M_PI * decimalInput)
-    let overlayCirclePath = UIBezierPath()
-    overlayCirclePath.addArc(withCenter: centerPoint, radius: overlayCircleRadius, startAngle: start, endAngle: end , clockwise: true)
-    //overlayCirclePath.addLine(to: centerPoint)
-    
-    let overlayColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
-    overlayColor.setFill()
-    let headPath = UIBezierPath()
-    headPath.addArc(withCenter: CGPoint(x: 130,y:headY), radius: headRadius, startAngle: start, endAngle: start + 2 * pi * 0.5 , clockwise: false)
-    headPath.fill()
-    
+
     let lightCirclePath = UIBezierPath()
-    lightCirclePath.addArc(withCenter: centerPoint, radius: overlayCircleRadius, startAngle: 0, endAngle: 2 * pi , clockwise: true)
-    let lighter = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+    lightCirclePath.addArc(withCenter: centerPoint, radius: innerCircleRadius, startAngle: 0, endAngle: 2 * pi , clockwise: true)
+    let lighter = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
     lighter.setFill()
     lightCirclePath.fill()
+    
+    let glucoseRangeColor = UIColor(displayP3Red: 251/255.0, green: 0, blue: 105/255.0, alpha: 0.6)
+    glucoseRangeColor.setStroke()
+    glucoseRangeColor.setFill()
+    //innerCirclePath.stroke()
+    
+    let outerCirclePath = UIBezierPath()
+    outerCirclePath.lineWidth = 4
+    outerCirclePath.addArc(withCenter: centerPoint, radius: fillCircleRadius, startAngle: 0, endAngle: 180/180  * pi , clockwise: true)
+    
+    
+    let point = getPoint(center: centerPoint, radius: innerCircleRadius, degree: 180)
+    outerCirclePath.addLine(to: point)
+    outerCirclePath.move(to: point)
+    outerCirclePath.addArc(withCenter: centerPoint, radius: innerCircleRadius, startAngle: 180/180 * pi, endAngle: 0, clockwise: false)
+    outerCirclePath.addLine(to: getPoint(center: centerPoint, radius: fillCircleRadius, degree: 0))
+    outerCirclePath.fill()
+    
+    let beginingHeadPath = UIBezierPath()
+    let beginingCenter = getPoint(center: centerPoint, radius: innerCircleRadius + (fillCircleRadius - innerCircleRadius) / 2, degree: 0)
+    beginingHeadPath.addArc(withCenter: beginingCenter, radius: (fillCircleRadius - innerCircleRadius) / 2, startAngle: pi, endAngle: 0, clockwise: true)
+    beginingHeadPath.fill()
+    
+    
+    let endingHeadPath = UIBezierPath()
+    let endingCenter = getPoint(center: centerPoint, radius: innerCircleRadius + (fillCircleRadius - innerCircleRadius) / 2, degree: 180)
+    endingHeadPath.addArc(withCenter: endingCenter, radius: (fillCircleRadius - innerCircleRadius) / 2, startAngle: pi, endAngle: 0, clockwise: true)
+    endingHeadPath.fill()
+    
+    
+    UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
 }
 
 func drawContent(input:Double,unit:String){
@@ -101,13 +118,18 @@ func getImage() -> UIImage{
 
 
 
-
+func getPoint(center:CGPoint,radius:CGFloat,degree:CGFloat) -> CGPoint{
+    let x:CGFloat = center.x + radius * cos(degree * pi / 180)
+    let y:CGFloat = center.y + radius * sin(degree * pi / 180)
+    return CGPoint(x: x, y: y)
+}
 
 
 
 drawBackground()
-drawContent(input: 0.2, unit: "mg/dL")
-getImage()
+
+//drawContent(input: 0.2, unit: "mg/dL")
+//getImage()
 
 
 
